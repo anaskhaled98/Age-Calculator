b8678a1dd515642e383235c725796fe4a6fc1d76
anas khaled <anaskhaled98@gmail.com>
2024-02-15 21:43:48 +0200

    Edit warning message to be updated automatic when (card__input--error) class added to the input and edit validation to not work with regex but work with regular conitions.

--- script.js
+++ script.js
@@ -1,30 +1,84 @@
 const inputs = document.querySelectorAll("[type='number']");
 const calculateButton = document.querySelector(".card__button");
-const dayElement = document.querySelector("[name='day']");
-const monElement = document.querySelector("[name='month']");
-const yearElement = document.querySelector("[name='year']");
-const resultValue = document.querySelector(".card__resultValue");
 const currentYear = new Date().getFullYear();
+
+/*
+// Validation by Regex
+
 const regexForDays = /^(0?[1-9]|[1-2][0-9]|3[0-1])$/;
 const regexForMonth = /^(0?[1-9]|1[0-2])$/;
 const regexForYears = new RegExp(`^([1-${currentYear}])$`);
 
-// Validation
 
 const validationFunction = (element,regex) =>
 {
     if(!regex.test(element.value)) 
     {   
-        element.classList.add("warningBorder");
-        element.nextElementSibling.classList.add("warningMessage");
+        element.classList.add("card__input--error");
         return false;
     }
     else {
-        element.classList.remove("warningBorder");
-        element.nextElementSibling.classList.remove("warningMessage");
+        element.classList.remove("card__input--error");
         return true;
     }
 }
+*/
+
+// Validation 
+const validateDay = (day) => {
+    if(day && day > 0 && day <= 31) {
+    return true;
+}
+}
+
+const validateMonth = (month) => {
+    if(month && month > 0 && month <= 12) {
+    return true;
+}
+}
+
+const validateYear = (year) => {
+    if(year && year > 0 && year < currentYear) {
+        return true;
+    }
+}
+
+const isDateValid = (dayElement,monthElement,yearElement) => {
+ let isValid = [false,false,false];
+
+ if(!validateDay(dayElement.value)) {
+    dayElement.classList.add("card__input--error");
+ }
+ else  {
+    isValid[0] = true;
+    dayElement.classList.remove("card__input--error");
+ }
+ 
+ if(!validateMonth(monthElement.value)) {
+    monthElement.classList.add("card__input--error");
+ }
+ else  {
+    isValid[1] = true;
+    monthElement.classList.remove("card__input--error");
+ }
+
+ if(!validateYear(yearElement.value)) {
+    yearElement.classList.add("card__input--error");
+ }
+ else  {
+    isValid[2] = true;
+    yearElement.classList.remove("card__input--error");
+ }
+
+
+ return isValid.every ((item) => {
+    if(item) return true;
+ })
+
+}
+
+
+
 
  // Get Date
 function calculateAge(year,month,day) {
@@ -42,20 +96,36 @@ function calculateAge(year,month,day) {
 }
 
 const onClickHandler = () => {
+    const dayElement = document.querySelector("[name='day']");
+    const monthElement = document.querySelector("[name='month']");
+    const yearElement = document.querySelector("[name='year']");
+    const resultValue = document.querySelector(".card__resultValue");
+    
+    /* 
     validationFunction(dayElement,regexForDays);
     validationFunction(monElement,regexForMonth);
     validationFunction(yearElement,regexForYears);
 
 
     if(validationFunction(dayElement,regexForDays) && validationFunction(monElement,regexForMonth)){  
-    resultValue.textContent = calculateAge(yearElement.value,monElement.value,dayElement.value);
+
+        resultValue.textContent = calculateAge(yearElement.value,monElement.value,dayElement.value);
     }
+    */
+
+    if(!isDateValid(dayElement,monthElement,yearElement)) {
+        resultValue.textContent = "--"
+        return;
+    }
+
+    resultValue.textContent = calculateAge(yearElement.value,monthElement.value,dayElement.value);
 
     };
     inputs.forEach((item) => {
     item.addEventListener("keydown", (event) => {
         if(event.key === "Enter") onClickHandler();
     })
+
 })
 
 calculateButton.addEventListener("click", onClickHandler);
